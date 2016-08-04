d4ba31525c5ee11f33183e822dcf445eb0bf0aec
Afzal7 <md.afzal1234@gmail.com>
2016-08-04 16:16:18 +0530

    Changes in Angular Assignment

--- Angular Assignment/app.js
+++ Angular Assignment/app.js
@@ -1,22 +1,67 @@
-var app = angular.module('myApp',['ngRoute']);
+var app = angular.module('myApp',['ngRoute','ngAnimate','ui.bootstrap']);
 
-app.controller('myCtrl',function($rootScope, $scope){
+app.controller('myCtrl',function($rootScope, $scope, $uibModal,$filter){
+	$scope.user = 'Bruce';
+
 	$scope.show_compose = false;
 	$rootScope.show_delete = false;
 
-	$scope.mail_data = JSON.parse(data);
+	$scope.mail_data = data.Mails;
+	$scope.mail_list = [];
+	$scope.mail_list_type = '';
 
-	$scope.menu_options = ["Inbox" , "Sent" , "Spam" , "Label"];
+
+	$scope.menu_options = [{id:0,name:"Inbox"} , {id:1,name:"Sent"} , {id:2,name:"Spam"} , {id:3,name:"Label"}];
 
 	$scope.new_name = $scope.new_subject = $scope.new_message = '';
 	$scope.mail = '';
 
-	$scope.open_mail = function(index){
-		$scope.mail = $rootScope.mail_list[index];
+	$scope.get_mails = function(mail_type){
+		if (mail_type == 'Inbox'){
+			$scope.filtered_mail_data = $scope.filterMails($scope.mail_data,'reciever');
+			$scope.filtered_mail_data = $scope.tagsFilter($scope.filtered_mail_data,'spam',true);
+		}
+		else if (mail_type == 'Sent'){
+			$scope.filtered_mail_data =  $scope.filterMails($scope.mail_data,'sender');
+		}
+		else if (mail_type == 'Spam'){
+			$scope.filtered_mail_data = $scope.filterMails($scope.mail_data,'reciever');
+			$scope.filtered_mail_data = $scope.tagsFilter($scope.filtered_mail_data,'spam',false);
+		}
+		$scope.mail_list = $scope.filtered_mail_data;
+		return $scope.filtered_mail_data;
 	};
 
-	$scope.close_mail = function(){
-		$scope.mail = '';	
+	$scope.filterMails = function(arr,type){
+		new_arr = [];
+        angular.forEach(arr, function (item,i) {
+        	flag = true;
+        	if (item[type] != $scope.user)
+        	{
+        		flag = false;
+        	}
+        	if (flag)
+        	{
+        		new_arr.push(item);
+        	}
+        });
+        return new_arr;
+	};
+
+	$scope.tagsFilter = function(arr, tag, flag_value){
+		new_arr = [];
+		angular.forEach(arr, function (item) {
+			flag = flag_value;
+			angular.forEach(item.tags, function(t) {  
+    		if (t == tag) {
+    			flag = !flag;
+    		}
+    		});
+	    	if (flag) {
+				new_arr.push(item);
+	      	}
+		});
+		return new_arr;
 	};
 
 	$rootScope.selected_mail = [];
@@ -42,10 +87,15 @@ app.controller('myCtrl',function($rootScope, $scope){
 		$scope.show_compose = true;
 	};
 
-	$scope.send_mail = function(){
-		new_mail = {"subject":$scope.new_subject,"text":$scope.new_message,"sender":$scope.new_name};
-		$scope.mail_data['Sent'].push(new_mail);
-		$scope.show_compose = false;
+	$rootScope.$on('send_the_mail',function(event, mail_data){
+		$scope.send_mail(mail_data);
+	});
+
+	$scope.send_mail = function(mail_data){
+		new_mail = {subject:mail_data[1], text:mail_data[2], sender:$scope.user, reciever:mail_data[0], tags:[]};
+		$scope.mail_data.push(new_mail);
+
+		$scope.display_message("Message Sent Successfully.");
 	};	
 
 	$scope.cancel_mail = function(){
@@ -55,10 +105,40 @@ app.controller('myCtrl',function($rootScope, $scope){
 	$scope.delete = function(){
 		for (i=0;i<$rootScope.selected_mail.length;i++)
 		{
+			$scope.mail_data.splice($scope.mail_data.indexOf($rootScope.selected_mail[i]),1);
 			$rootScope.mail_list.splice($rootScope.mail_list.indexOf($rootScope.selected_mail[i]),1);
 		}
 		$rootScope.selected_mail = [];
-		$rootScope.show_delete = false;	
+		$rootScope.show_delete = false;
+		$scope.display_message("Message Deleted Successfully.");
+	};
+	$scope.open_compose_mail = function(){
+		$uibModal.open({
+			animation : true,
+			templateUrl : 'compose_mail_template.html',
+			controller : 'modalCtrl',
+			size:'lg'
+		});
+	};
+	$scope.open_mail_content = function(index){
+		$uibModal.open({
+			animation : true,
+			templateUrl : 'mail_content.html',
+			controller : 'mail_content_controller',
+			resolve: {
+				index : index
+			}
+		});
+	};
+	$scope.display_message = function(message){
+		$uibModal.open({
+			animation : true,
+			templateUrl : 'message_template.html',
+			controller : 'display_message_controller',
+			resolve: {
+				message : function(){ return message; }
+			}
+		});
 	};
 });
 
@@ -69,23 +149,30 @@ app.config(function($routeProvider){
 	})
 	.when("/Inbox",{
 		templateUrl : "display_mail.html",
-		controller : "select_mail_list"
+		controller : "show_inbox"
 	})
 	.when("/Sent",{
 		templateUrl : "display_mail.html",
-		controller : "select_mail_list"
+		controller : "show_sent"
 	})
 	.when("/Spam",{
 		templateUrl : "display_mail.html",
-		controller : "select_mail_list"
-	})
-	.when("/Label",{
-		templateUrl : "display_mail.html",
-		controller : "select_mail_list"
+		controller : "show_spam"
 	})
 	.otherwise({
 		templateUrl : "/error.html"
 	});
+});
+app.controller("show_inbox",function($rootScope,$scope,$route){
+	$rootScope.mail_list = $scope.get_mails('Inbox');
+});
+
+app.controller("show_sent",function($rootScope,$scope,$route){
+	$rootScope.mail_list = $scope.get_mails('Sent');
+});
+
+app.controller("show_spam",function($rootScope,$scope,$route){
+	$rootScope.mail_list = $scope.get_mails('Spam');
 });
 
 app.controller("select_mail_list",function($rootScope,$scope,$route){
@@ -93,4 +180,30 @@ app.controller("select_mail_list",function($rootScope,$scope,$route){
 	$rootScope.mail_list = $scope.mail_data[$scope.menu_options[index]];
 	$rootScope.selected_mail = [];
 	$rootScope.show_delete = false;	
+});
+
+app.controller('modalCtrl',function($scope,$rootScope,$uibModalInstance){
+	$scope.send_mail = function(){
+		mail_data = [$scope.new_name, $scope.new_subject, $scope.new_message];
+		$rootScope.$emit("send_the_mail",mail_data);
+		$scope.close_modal()
+	}
+	$scope.close_modal = function(){
+		$uibModalInstance.close();
+	}
+});
+
+app.controller('mail_content_controller',function($scope,$rootScope,index, $uibModalInstance){
+	$scope.mail = $rootScope.mail_list[index];
+
+	$scope.close_modal =function(){
+		$uibModalInstance.close();
+	}
+});
+
+app.controller('display_message_controller',function($scope, message, $uibModalInstance, $timeout){
+	$scope.message = message;
+	$timeout(function () {
+		$uibModalInstance.close();	
+	}, 2000);
 });
